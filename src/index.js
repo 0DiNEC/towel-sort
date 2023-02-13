3b64d93d9040591f6901c0bdcc44641fd63ad90a
0DiNEC <as.odinson@gmail.com>
2023-02-13 19:51:00 +0300

    fix: Update to return empty

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-module.exports = function towelSort (matrix) {
-  let towelMatrix = [];
-  let indexRow = 0;
-  for (const row of matrix) {
-    if (indexRow % 2 === 0)
-    for (let i = 0; i < row.length; i++) {
-      towelMatrix.push(row[i]);
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    let towelMatrix = [];
+    let indexRow = 0;
+    for (const row of matrix) {
+      if (indexRow % 2 === 0)
+        for (let i = 0; i < row.length; i++) {
+          towelMatrix.push(row[i]);
+        }
+      else
+        for (let i = row.length - 1; i >= 0; i--) {
+          towelMatrix.push(row[i]);
+        }
+      indexRow++
     }
-    else
-    for (let i = row.length-1; i >= 0; i--) {
-      towelMatrix.push(row[i]);
-    } 
-    indexRow++
+    return towelMatrix;
   }
-  return towelMatrix;
+  else return [];
 }
